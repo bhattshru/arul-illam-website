6ab5f0118bb73a23cc501071c68d68c4a1c8a14f
Madhan Balachandar <Madhan.Balachandar@siriuscom.com>
2018-11-08 18:18:07 +0530

    Home page - Initial Commit

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -9,7 +9,14 @@ export class HomePageComponent implements OnInit {
 
  contactViewType = "CONTACT_WITH_MAP";
  
-  constructor() { }
+  ourStoryContent1:string;
+  ourStoryContent2:string;
+  constructor() { 
+
+    this.ourStoryContent1 = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.';
+    this.ourStoryContent2 = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary';
+
+  }
 
   ngOnInit() {
   }
