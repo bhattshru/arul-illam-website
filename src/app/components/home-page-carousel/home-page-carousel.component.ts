d5171dc8bfcf2b68286b6adcf090b7288aec5f3c
Madhan Balachandar <Madhan.Balachandar@siriuscom.com>
2018-12-09 13:23:50 +0530

    carousel integration

--- src/app/components/home-page-carousel/home-page-carousel.component.ts
+++ src/app/components/home-page-carousel/home-page-carousel.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Response, Http, Headers, RequestOptions } from '@angular/http';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Input } from '@angular/core';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-home-page-carousel',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageCarouselComponent implements OnInit {
 
-  constructor() { }
+  private _aboutUsContentURL = '//arulillam-cms.herokuapp.com/carousel-data';
+  image1: string;
+  image2: string;
+  image3: string;
 
-  ngOnInit() {
+  constructor(private http: Http) {
+    
   }
 
+  public ping() {
+
+    let headers = new Headers();
+    headers.append("Authorization", "Basic " + btoa("siva:P@ssw0rd"));
+    headers.append("Content-Type", "application/json");
+
+    let body = null;
+    let options = new RequestOptions({ headers: headers });
+    this.http.get(this._aboutUsContentURL, options).subscribe(response => {
+      console.log(response);
+      let jsonData = response.json();
+      
+        this.image1 = jsonData[0].imageurl;
+        this.image2 = jsonData[1].imageurl;
+        this.image3 = jsonData[2].imageurl;
+
+    }, err => {
+      console.log("User authentication failed!");
+    });
+  }
+
+  ngOnInit() {
+    this.ping();
+  }
 }
