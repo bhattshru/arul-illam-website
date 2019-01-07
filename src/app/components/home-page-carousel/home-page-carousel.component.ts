16bf7fd4115a5543fa1110961aeffe5cd72bcf8a
Madhan Balachandar <Madhan.Balachandar@siriuscom.com>
2019-01-07 11:47:43 +0530

    Home page carousel - loader integration

--- src/app/components/home-page-carousel/home-page-carousel.component.ts
+++ src/app/components/home-page-carousel/home-page-carousel.component.ts
@@ -4,6 +4,7 @@ import { Response, Http, Headers, RequestOptions } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Input } from '@angular/core';
 import { TouchSequence } from 'selenium-webdriver';
+import { LoaderService } from 'src/app/services/loader/loader.service';
 
 @Component({
   selector: 'app-home-page-carousel',
@@ -16,12 +17,15 @@ export class HomePageCarouselComponent implements OnInit {
   image1: string;
   image2: string;
   image3: string;
+  
 
-  constructor(private http: Http) {
-    
+  constructor(private http: Http, private loaderService: LoaderService) {
+
   }
 
   public ping() {
+
+    this.loaderService.show();
 
     let headers = new Headers();
     headers.append("Authorization", "Basic " + btoa("siva:P@ssw0rd"));
@@ -36,9 +40,11 @@ export class HomePageCarouselComponent implements OnInit {
         this.image1 = jsonData[0].imageurl;
         this.image2 = jsonData[1].imageurl;
         this.image3 = jsonData[2].imageurl;
-
+        
+        this.loaderService.hide();
     }, err => {
       console.log("User authentication failed!");
+      this.loaderService.hide();
     });
   }
 
